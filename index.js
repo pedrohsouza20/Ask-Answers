dd7e2b0d01b16d84ffc05a99e3859b3a6226ec78
Pedro <contato.pedromoc@gmail.com>
2022-03-22 21:42:29 -0300

    adicionada ordenação por data utilizando ID na listagem das perguntas

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  Ask.findAll({ raw: true }).then((asks) => {
+  Ask.findAll({ raw: true, order: [["id", "DESC"]] }).then((asks) => {
     res.render("index", {
       asks: asks,
     });
